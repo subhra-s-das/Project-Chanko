ff9452340a959c0bc8233f5f2a72877694ff2703
sudas <subhra.s.das@gmail.com>
2015-09-16 16:57:18 +0530

    Check-in

---
+++ src/test/java/sudas/Git/Inheritance01.java
@@ -0,0 +1,64 @@
+package sudas.Git;
+
+public class Inheritance01 {
+	public static void main(String[] args) {
+
+		// creating an object of subclass Dog
+		Dog d = new Dog(); // -> The default constructor of Dog is called //
+							// here.
+//		Boy b = new Boy();
+	}
+
+}
+
+/** Constructors are not inherited like instance variable and methods
+ rather they need to be called
+ In the below class person I have three constructors for PErson class
+ The default constructor is Person(), if the default constructor is commented out the subclass Boy gives and
+error "Implicit super constructor Person() is undefined for default constructor. Must define an explicit constructor"**/
+
+class Person {
+	String name;
+	int age;
+
+	// Default constructor
+//	Person() {
+//
+//	}
+
+	// Argument constructor parameterized constructor
+	Person(String name) {
+
+	}
+
+	Person(String name, int age) {
+
+	}
+
+}
+
+//class Boy extends Person {
+//
+//}
+
+/*
+ * Here I have a class animal. The DOg class inherits the animal class Both
+ * class has default constructors. When I create an object of inherited class
+ * and call the inherited method class both super class and subclass
+ * constructors are called. Because the dog constructor knows how to initialize
+ * the the default constructor of Dog class where as the Animal class
+ * constructor knows how to initialize Animal default constructor. As the dog
+ * class extends Animal class the animal class default constructor is called.
+ * Remember Constructors are not Inherited
+ */
+class Animal {
+	public Animal() {
+		System.out.println("Animal : Super constructor");
+	}
+}
+
+class Dog extends Animal {
+	public Dog() {
+		System.out.println("Dog : Sub constructor");
+	}
+}
