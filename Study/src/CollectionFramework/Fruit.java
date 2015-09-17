38709cb8e7b9c29b041f962d6d6663aaec185aa2
subhra-s-das <subhra.s.das@gmail.com>
2015-09-17 22:56:17 +0530

    CollectionFramework
    CollectionFramework

---
+++ Study/src/CollectionFramework/Fruit.java
@@ -0,0 +1,30 @@
+package CollectionFramework;
+
+public class Fruit {
+	String name;
+	String colour;
+	int quantity;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getColour() {
+		return colour;
+	}
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
