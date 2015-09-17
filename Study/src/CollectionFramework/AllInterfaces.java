a6204e1440c1daef647734aa9c0493706690cace
subhra-s-das <subhra.s.das@gmail.com>
2015-09-17 23:00:07 +0530

    test from office Dell
    test from office Dell

--- Study/src/CollectionFramework/AllInterfaces.java
+++ Study/src/CollectionFramework/AllInterfaces.java
@@ -8,7 +8,7 @@ import java.util.Comparator;
 public class AllInterfaces {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub for test
 
 	}
 
