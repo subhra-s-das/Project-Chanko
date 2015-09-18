999982d4a26f6081315fa5bb47cc4da880c82a42
subhra-s-das <subhra.s.das@gmail.com>
2015-09-18 16:23:11 +0530

    changes from dell
    changes from dell

---
+++ Study/src/CollectionFramework/OFruit.java
@@ -0,0 +1,45 @@
+package CollectionFramework;
+
+import java.util.List;
+
+public class OFruit implements Comparable<OFruit> {
+	String name;
+	String colour;
+	int quantity;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getColour() {
+		return colour;
+	}
+
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int compareTo(OFruit fruit) {
+	return this.name.compareTo(fruit.getName());
+		
+	}
+
+}
