b9d26c22b68ef1f36b78875b4916cacd1708202f
subhra-s-das <subhra.s.das@gmail.com>
2015-09-21 23:14:19 +0530

    From mac

--- Study/src/sudas/study/example/one/DateExample.java
+++ Study/src/sudas/study/example/one/DateExample.java
@@ -10,7 +10,7 @@ public class DateExample {
 	public static void main(String[] args) {
 		ChangeDate dateuse = new ChangeDate();
 		dateuse.EpochToDateTime();
-//		dateuse.dateTimeToEpoch();
+		dateuse.dateTimeToEpoch();
 
 	}
 
@@ -24,18 +24,18 @@ class ChangeDate {
 		System.out.println(formatDate);
 	}
 	
-//	public void dateTimeToEpoch()
-//	{
-//		String dateTime="21/09/2015 08:10:00";
-//		SimpleDateFormat format = new SimpleDateFormat("dateTime");
-//try {
-//	Date epoch = format.parse(dateTime);
-//	System.out.println();
-//	
-//} catch (ParseException e) {
-//	// TODO Auto-generated catch block
-//	e.printStackTrace();
-//}
-//	}
+	public void dateTimeToEpoch()
+	{
+		String dateTime="21/09/2015 08:10:00";
+		SimpleDateFormat format = new SimpleDateFormat("dd/mm/yyyy hh:mm:ss");
+try {
+	Date epoch = format.parse(dateTime);
+	System.out.println(epoch.getTime()/1000);
+	
+} catch (ParseException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
+	}
 }
 
--- _pasted_code_/src/snippet/Snippet.java
+++
@@ -1,8 +0,0 @@
-package snippet;
-
-public class Snippet {
-	public static void main(String[] args) {
-		C:\Users\sudas\workspace\EE
-	}
-}
-
