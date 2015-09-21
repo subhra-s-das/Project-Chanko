6ed5ef92c1a682a14f9cd87142c927f598dd1980
subhra-s-das <subhra.s.das@gmail.com>
2015-09-21 23:15:57 +0530

    mac

--- Study/src/sudas/study/example/one/DateExample.java
+++ Study/src/sudas/study/example/one/DateExample.java
@@ -17,25 +17,23 @@ public class DateExample {
 }
 
 class ChangeDate {
-	
+
 	public void EpochToDateTime() {
 		DateFormat format = new SimpleDateFormat("dd/mm/yyyy hh:mm:ss");
 		String formatDate = format.format(new Date().getTime());
 		System.out.println(formatDate);
 	}
-	
-	public void dateTimeToEpoch()
-	{
-		String dateTime="21/09/2015 08:10:00";
+
+	public void dateTimeToEpoch() {
+		String dateTime = "21/09/2015 08:10:00";
 		SimpleDateFormat format = new SimpleDateFormat("dd/mm/yyyy hh:mm:ss");
-try {
-	Date epoch = format.parse(dateTime);
-	System.out.println(epoch.getTime()/1000);
-	
-} catch (ParseException e) {
-	// TODO Auto-generated catch block
-	e.printStackTrace();
-}
+		try {
+			Date epoch = format.parse(dateTime);
+			System.out.println(epoch.getTime() / 1000);
+
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
-
